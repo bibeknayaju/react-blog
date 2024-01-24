1141f1ec7b0208197c23faa688b7bf3174ca440d
Bibek Nayaju <bibeknayaju@github.com>
2024-01-24 13:48:48 +0545

    create signin api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import FooterCom from "./components/FooterCom";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <FooterCom />
     </BrowserRouter>
   );
 }
