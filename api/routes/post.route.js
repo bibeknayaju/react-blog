9e3d235a5182c5da72efeaac2e2e5c86e11f9068
Bibek Nayaju <bibeknayaju@github.com>
2024-01-30 18:35:43 +0545

    add show more funcationalities

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express();
 import { verifyToken } from "../utils/verifyUser.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 router.post("/create-post", verifyToken, createPost);
+router.get("/getPosts", getPosts);
 
 export default router;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/Dashposts";
 
 function Dashboard() {
   const location = useLocation();
@@ -13,13 +14,15 @@ function Dashboard() {
     if (tabFromUrl) setTab(tabFromUrl);
   }, [location.search]);
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
+    <div className="min-h-screen flex flex-col md:flex-row">
       {/* for sidebar */}
       <div className="md:w-56">
         <DashSidebar />
       </div>
       {/* for dash right side */}
       {tab === "profile" && <DashProfile />}
+      {/* for post */}
+      {tab === "posts" && <DashPosts />}
     </div>
   );
 }
