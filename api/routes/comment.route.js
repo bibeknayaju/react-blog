c2db491b8ca237265800601e13299821653b9aff
Bibek Nayaju <bibeknayaju@github.com>
2024-01-31 18:58:32 +0545

    Add like functionality to comments

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,10 +4,12 @@ const router = express.Router();
 import {
   createComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.post("/create-comment", verifyToken, createComment);
 router.get("/get-comments/:postId", getPostComments);
+router.put("/like-comment/:commentId", verifyToken, likeComment);
 
 export default router;
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, onLikeFunction }) {
+  const { currentUser } = useSelector((state) => state.user);
   const [user, setUser] = useState({});
   useEffect(() => {
     const getUser = async () => {
@@ -30,14 +33,33 @@ export default function Comment({ comment }) {
       <div className="flex-1">
         <div className="flex items-center mb-1">
           <span className="font-bold mr-1 text-xs truncate">
-            {user ? `@${user.username}` : "anonymous user"}
+            {user ? `@${user?.username}` : "anonymous user"}
           </span>
           <span className="text-gray-500 text-xs">
-            {moment(comment.createdAt).fromNow()}
+            {moment(comment?.createdAt).fromNow()}
           </span>
         </div>
 
-        <p className="text-gray-500 pb-2">{comment.content}</p>
+        <p className="text-gray-500 pb-2">{comment?.content}</p>
+
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            type="button"
+            onClick={() => onLikeFunction(comment._id)}
+            className={`text-gray-400 hover:text-blue-500  ${
+              currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              "!text-blue-500"
+            } `}>
+            <FaThumbsUp className="text-sm" />
+          </button>
+          <p className="text-gray-400">
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes +
+                " " +
+                (comment.numberOfLikes === 1 ? "like" : "likes")}
+          </p>
+        </div>
       </div>
     </div>
   );
