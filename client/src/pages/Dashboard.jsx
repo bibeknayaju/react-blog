288e38584a49490b89791918d41e29dabaaad42e
Bibek Nayaju <bibeknayaju@github.com>
2024-02-01 13:50:04 +0545

    Remove vite.svg and reorganize imports in Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,37 +3,36 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
-import DashboardComp from "../components/DashboardComp";
-import DashComments from "../components/DashComments";
 import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
-function Dashboard() {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-    if (tabFromUrl) setTab(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
   }, [location.search]);
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      {/* for sidebar */}
       <div className="md:w-56">
+        {/* Sidebar */}
         <DashSidebar />
       </div>
-      {/* for dash right side */}
+      {/* profile... */}
       {tab === "profile" && <DashProfile />}
-      {/* for post */}
+      {/* posts... */}
       {tab === "posts" && <DashPosts />}
-      {/* for users */}
+      {/* users */}
       {tab === "users" && <DashUsers />}
-      {/* for comments */}
+      {/* comments  */}
       {tab === "comments" && <DashComments />}
-      {/* for dashboard */}
+      {/* dashboard comp */}
       {tab === "dash" && <DashboardComp />}
     </div>
   );
 }
-
-export default Dashboard;
