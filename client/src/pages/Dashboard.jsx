f6bb678522047905959f3cb0b1303d5c9328b6aa
Bibek Nayaju <bibeknayaju@github.com>
2024-02-01 12:22:50 +0545

    final

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import DashSidebar from "../components/DashSidebar";
-import DashProfile from "../components/DashProfile";
-import DashPosts from "../components/DashPosts";
-import DashUsers from "../components/DashUsers";
-import DashComments from "../components/DashComments";
-import DashboardComp from "../components/DashboardComp";
+import DashSidebar from "@components/DashSidebar";
+import DashProfile from "@components/DashProfile";
+import DashPosts from "@components/DashPosts";
+import DashUsers from "@components/DashUsers";
+import DashComments from "@components/DashComments";
+import DashboardComp from "@components/DashboardComp";
 
 function Dashboard() {
   const location = useLocation();
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@components", replacement: "/src/components" }],
+  },
   server: {
     proxy: {
       "/api": {
